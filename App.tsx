3994cf922d0011c4b372baac99000c4f4aa487fd
olivia <olivia@github.com>
2023-03-19 21:48:33 -0400

    Get intersecting working again

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './style.css';
 
-const [height, width, dx, dy] = [240, 400, 20, 20];
+const [height, width, dx, dy] = [480, 800, 40, 40];
 const [minx, maxx, miny, maxy] = [0, width / dx, 0, height / dy];
 
 const randIdx = () =>
@@ -80,6 +80,7 @@ const lineIdxIntersects = ([l1s, l1e], [l2s, l2e]) => {
   const denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
   // parallel or coincident
   if (denominator === 0) {
+    console.log(denominator);
     return pointIsOnLine(l2s, [l1s, l1e]) || pointIsOnLine(l2e, [l1s, l1e]);
   }
   const px =
@@ -88,13 +89,21 @@ const lineIdxIntersects = ([l1s, l1e], [l2s, l2e]) => {
   const py =
     ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) /
     denominator;
-
+  console.log(px, py);
   return (
     Math.min(x1, x2) <= px &&
     Math.max(x1, x2) >= px &&
     Math.min(y1, y2) <= py &&
-    Math.max(y1, y2) >= py
+    Math.max(y1, y2) >= py &&
+    Math.min(x3, x4) <= px &&
+    Math.max(x3, x4) >= px &&
+    Math.min(y3, y4) <= py &&
+    Math.max(y3, y4) >= py
   );
+};
+
+const prettyPrint = ([l1s, l1e], [l2s, l2e]) => {
+  return [l1s, l1e, l2s, l2e].map(idxToXY);
 };
 
 const isParallel = ([l1s, l1e], [l2s, l2e]) => {
@@ -104,38 +113,42 @@ const isParallel = ([l1s, l1e], [l2s, l2e]) => {
   const denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
   return denominator === 0;
 };
+const pDir = ['NW', 'NE', 'SE', 'SW', 'N', 'E', 'S', 'W'];
 
-const randomWalk = ({ index, maxStep, dhRatio }) => {
+const randomWalk = ({ index, maxStep, dhRatio, skipList = [] }) => {
   const [isDiagonal, dir, magnitude] = [
     Math.random() < dhRatio,
     Math.round(Math.random() * 3),
     1 + Math.round(Math.random() * (maxStep - 1)),
   ];
-  let offset;
-  if (isDiagonal) {
-    const offsets = [
-      [-1, 1],
-      [1, 1],
-      [1, -1],
-      [-1, -1],
-    ].map(([x, y]) => [x * magnitude, y * magnitude]);
-    offset = offsets[dir];
-  } else {
-    const offsets = [
-      [0, 1],
-      [1, 0],
-      [0, -1],
-      [-1, 0],
-    ].map(([x, y]) => [x * magnitude, y * magnitude]);
-    offset = offsets[dir];
-  }
+  const diagonalOffsets = [
+    [-1, 1],
+    [1, 1],
+    [1, -1],
+    [-1, -1],
+  ];
+  const cardinalOffsets = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+  const offsets = diagonalOffsets
+    .concat(cardinalOffsets)
+    .map(([x, y]) => [x * magnitude, y * magnitude]);
+
+  const nonskippedOffset = offsets.filter((_, i) => skipList.indexOf(i) === -1);
+
+  const offset =
+    nonskippedOffset[Math.round(Math.random() * (nonskippedOffset.length - 1))];
+
   const oldXY = idxToXY(index);
   let newXY = [
     Math.min(maxx - 1, Math.max(minx, oldXY[0] + offset[0])),
     Math.min(maxy - 1, Math.max(miny, oldXY[1] + offset[1])),
   ];
 
-  if (isDiagonal) {
+  if (offsets.indexOf(offset) < 4) {
     const maxMagnitude = Math.min(
       Math.abs(newXY[0] - oldXY[0]),
       Math.abs(newXY[1] - oldXY[1])
@@ -146,7 +159,7 @@ const randomWalk = ({ index, maxStep, dhRatio }) => {
     ];
   }
 
-  return xyToIdx(newXY as [number, number]);
+  return [offsets.indexOf(offset), xyToIdx(newXY as [number, number])];
 };
 
 const getLineArr = (prevPoints) => {
@@ -160,21 +173,22 @@ const getLineArr = (prevPoints) => {
 };
 
 // if can insert
-const canInsert = (l1s, prevPoints, shapenum) => {
+const canInsert = (le, prevPoints) => {
   if (prevPoints.length >= 2) {
     let found = 0;
     const lines = getLineArr(prevPoints);
-    if (
-      isParallel(
-        [l1s, prevPoints[prevPoints.length - 1]],
-        lines[lines.length - 1]
-      )
-    ) {
+    const startPoint = prevPoints[prevPoints.length - 1];
+    if (isParallel([startPoint, le], lines[lines.length - 1])) {
+      console.log('is parallel');
       return false;
     }
 
-    for (const l of lines.slice(0, -1)) {
-      if (lineIdxIntersects([l1s, prevPoints[prevPoints.length - 1]], l)) {
+    for (const l of lines) {
+      if (l[1] == startPoint) {
+        console.log('skipping for now');
+      } else if (lineIdxIntersects([startPoint, le], l)) {
+        console.log('interesect with');
+        console.log(prettyPrint([startPoint, le], l));
         return false;
       }
     }
@@ -194,11 +208,27 @@ export default function App() {
     } else {
       let iterations = 0;
       let found = false;
+      let skipList = [];
       while (iterations <= 100) {
-        const p = randomWalk({ index: prev, maxStep, dhRatio });
-        if (canInsert(p, prevPoints, shapeNum)) {
+        const [offsetAttempt, p] = randomWalk({
+          index: prev,
+          maxStep,
+          dhRatio,
+          skipList,
+        });
+        if (canInsert(p, prevPoints)) {
           return p;
         }
+        console.log(
+          `Tried ${pDir[offsetAttempt]} with node ${prev} w skiplist: ${skipList}`
+        );
+        if (skipList.length === 7) {
+          console.log('clearing skiplist');
+          skipList = [];
+        } else {
+          skipList.push(offsetAttempt);
+        }
+
         iterations++;
       }
       throw new Error('Exhausted iterations');
@@ -291,7 +321,7 @@ export default function App() {
 
   return (
     <div>
-      <svg width="400" height="240" style={{ background: 'grey' }}>
+      <svg width={width} height={height} style={{ background: 'grey' }}>
         <path d={hgrid} stroke="white" />
         <path d={vgrid} stroke="white" />
         {circles}
